a3b70bb3c24ccb592794524c1fd4c08fb2222570
RaffaeleVallifuoco <ra.vallifuoco@outlook.it>
2025-06-04 11:13:05 +0200

    CREATE : project package

---
+++ restfukl_fiven/src/main/java/restful/controller/Saluto.java
@@ -0,0 +1,10 @@
+package restful.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Saluto {
+
+}
